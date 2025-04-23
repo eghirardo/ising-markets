87c9eccc42102fc2ad352559c5e7e08909912733
eghirardo <edoardoghirardo@gmail.com>
2025-04-23 15:04:03 -0500

    update plots in curie-weiss model

---
+++ Code/plotting_utils.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+
+latex_textwidth = 455.24411
+
+# inspiration from https://jwalton.info/Embed-Publication-Matplotlib-Latex/
+def set_size(width=latex_textwidth, fraction=1, ratio=0.62):
+    """Set figure dimensions to avoid scaling in LaTeX.
+
+    Parameters
+    ----------
+    width: float, optional
+            Document textwidth or columnwidth in pts
+    fraction: float, optional
+            Fraction of the width which you wish the figure to occupy
+
+    Returns
+    -------
+    fig_dim: tuple
+            Dimensions of figure in inches
+    """
+    # Width of figure (in pts)
+    fig_width_pt = width * fraction
+
+    # Convert from pt to inches
+    inches_per_pt = 1 / 72.27
+
+
+    # Figure width in inches
+    fig_width_in = fig_width_pt * inches_per_pt
+    # Figure height in inches
+    fig_height_in = fig_width_in * ratio
+
+    fig_dim = (fig_width_in, fig_height_in)
+
+    return fig_dim
+
+def load_plot_style():
+    tex_fonts = {
+        # Use LaTeX to write all text
+        "text.usetex": True,
+        "font.family": "serif",
+        # Use 10pt font in plots, to match 10pt font in document
+        "axes.labelsize": 10,
+        "font.size": 10,
+        # Make the legend/label fonts a little smaller
+        "legend.fontsize": 8,
+        "xtick.labelsize": 8,
+        "ytick.labelsize": 8
+    }
+    plt.rcParams.update(tex_fonts)
